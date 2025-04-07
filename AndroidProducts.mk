ff053be61f8886b0b7fdd549af58d6952c269347
lynx <122455759+LynxSlash@users.noreply.github.com>
2025-04-07 19:20:35 +0500

    Adapted to halcyon

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a70q.mk
+    $(LOCAL_DIR)/halcyon_a70q.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_a70q-user \
-    lineage_a70q-userdebug \
-    lineage_a70q-eng
+    halcyon_a70q-user \
+    halcyon_a70q-userdebug \
+    halcyon_a70q-eng
