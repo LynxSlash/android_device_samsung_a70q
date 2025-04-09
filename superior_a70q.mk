aa157a9d3db52ba0ff0f5fd06def397ef3d6bd31
lynx <122455759+LynxSlash@users.noreply.github.com>
2025-04-09 07:53:08 +0500

    Adapted to superior

--- superior_a70q.mk
+++ superior_a70q.mk
@@ -11,11 +11,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from a70q device
 $(call inherit-product, device/samsung/a70q/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Superior stuff
+$(call inherit-product, vendor/superior/config/common.mk)
+
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_a70q
+PRODUCT_NAME := superior_a70q
 PRODUCT_DEVICE := a70q
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A705
