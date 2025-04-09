506adfd42bdcda45556518bc462088a49b2161d1
lynx <122455759+LynxSlash@users.noreply.github.com>
2025-04-09 07:54:25 +0500

    revert lineage

--- lineage_a70q.mk
+++ lineage_a70q.mk
@@ -11,13 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from a70q device
 $(call inherit-product, device/samsung/a70q/device.mk)
 
-# Inherit some common Superior stuff
-$(call inherit-product, vendor/superior/config/common.mk)
-
-TARGET_BOOT_ANIMATION_RES := 1080
+# Inherit some common Lineage stuff.
+ $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := superior_a70q
+PRODUCT_NAME := lineage_a70q
 PRODUCT_DEVICE := a70q
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := SM-A705
