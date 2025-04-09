aae18ddc5047a40724d4a78e51590e98dc8d2b25
lynx <122455759+LynxSlash@users.noreply.github.com>
2025-04-09 07:50:04 +0500

    Adapted to superior

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_a70q.mk
+    $(LOCAL_DIR)/superior_a70q.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_a70q-user \
-    lineage_a70q-userdebug \
-    lineage_a70q-eng
+    superior_a70q-user \
+    superior_a70q-userdebug \
+    superior_a70q-eng
