99e2d44158225f2847408dfa3357ba0c132fa98f
lynx <122455759+LynxSlash@users.noreply.github.com>
2025-04-12 07:51:54 +0500

    Adapted to derp

--- device.mk
+++ device.mk
@@ -24,6 +24,16 @@ DEVICE_PACKAGE_OVERLAYS += \
 PRODUCT_COPY_FILES += \
 	$(DEVICE_PATH)/audio/configs/mixer_paths_idp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_idp.xml
 
+# Derp Flags
+DERP_BUILDTYPE=Community
+DERP_VERSION_APPEND_TIME_OF_DAY=true
+EXTRA_UDFPS_ICONS=true
+TARGET_NOT_USES_BLUR=true
+TARGET_USES_PICO_GAPPS=true
+
+# Fingerprint
+TARGET_HAS_UDFPS := true
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
